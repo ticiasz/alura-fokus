ad83f7415f2fde7fcb4cfa1b43496eee00a03998
Letícia Matheus <let.matheus19@hotmail.com>
2023-10-31 17:41:13 -0300

    Adicionando a alteração das imagens ao clicar nos botões principais

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
 //quando o botão 'focoBt' receber o evento 'click', chamamos uma função
 focoBt.addEventListener('click', () => {
     //a função vai dar o valor 'foco' ao atributo 'data-contexto'
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png');
 });
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 });
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 });
