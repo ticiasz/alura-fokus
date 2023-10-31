4a9cdb88061c85ff10ba500bcd8e574954198673
Letícia Matheus <let.matheus19@hotmail.com>
2023-10-31 16:51:44 -0300

    Criando o evento para mudar a cor de fundo ao clicar nos botões principais

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const html = document.querySelector("html");
-const focoBt = document.querySelector(".app__card-button--foco");
-const curtoBt = document.querySelector(".app__card-button--curto");
-const longoBt = document.querySelector(".app__card-button--longo");
+const html = document.querySelector('html');
+const focoBt = document.querySelector('.app__card-button--foco');
+const curtoBt = document.querySelector('.app__card-button--curto');
+const longoBt = document.querySelector('.app__card-button--longo');
+
+//quando o botão 'focoBt' receber o evento 'click', chamamos uma função
+focoBt.addEventListener('click', () => {
+    //a função vai dar o valor 'foco' ao atributo 'data-contexto'
+    html.setAttribute('data-contexto', 'foco')
+})
+
+curtoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+})
+
+longoBt.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+})
