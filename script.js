1b25bdfea6e52b3cea446ddac628b391edc2d190
Letícia Matheus <let.matheus19@hotmail.com>
2023-10-31 16:43:20 -0300

    Adicionando referências para os botoes

---
+++ script.js
@@ -0,0 +1,4 @@
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
