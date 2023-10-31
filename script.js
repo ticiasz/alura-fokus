ac73fc56c464b1a4022b96371abfa9915abd8fef
Letícia Matheus <let.matheus19@hotmail.com>
2023-10-31 17:21:43 -0300

    Adição de ;

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const longoBt = document.querySelector('.app__card-button--longo');
 //quando o botão 'focoBt' receber o evento 'click', chamamos uma função
 focoBt.addEventListener('click', () => {
     //a função vai dar o valor 'foco' ao atributo 'data-contexto'
-    html.setAttribute('data-contexto', 'foco')
-})
+    html.setAttribute('data-contexto', 'foco');
+});
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-})
+    html.setAttribute('data-contexto', 'descanso-curto');
+});
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-})
+    html.setAttribute('data-contexto', 'descanso-longo');
+});
