c61ef9f1de11c332bca65bf2da691213e3dd4c0a
Ticia <let.matheus19@hotmail.com>
2023-11-01 11:38:51 -0300

    Adicionando os cases para os títulos dos outros botões

--- script.js
+++ script.js
@@ -35,7 +35,17 @@ function alterarContexto(contexto){
             <strong class="app__title-strong">mergulhe no que importa.</strong>
             `
             break;
-    
+        case 'descanso-curto':
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
         default:
             break;
     }
