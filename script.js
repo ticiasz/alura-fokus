8e58544bd610379edf3a0ef275d45f072b9340f3
Ticia <let.matheus19@hotmail.com>
2023-11-01 10:40:42 -0300

    Criando a função alterarContexto para otimizar as ações do botões

--- script.js
+++ script.js
@@ -7,16 +7,24 @@ const banner = document.querySelector('.app__image');
 //quando o botão 'focoBt' receber o evento 'click', chamamos uma função
 focoBt.addEventListener('click', () => {
     //a função vai dar o valor 'foco' ao atributo 'data-contexto'
-    html.setAttribute('data-contexto', 'foco');
-    banner.setAttribute('src', '/imagens/foco.png');
+    //html.setAttribute('data-contexto', 'foco');
+    //banner.setAttribute('src', '/imagens/foco.png');
+    alterarContexto('foco');
 });
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+    //html.setAttribute('data-contexto', 'descanso-curto');
+    //banner.setAttribute('src', '/imagens/descanso-curto.png')
+    alterarContexto('descanso-curto');
 });
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    //html.setAttribute('data-contexto', 'descanso-longo');
+    //banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo');
 });
+
+function alterarContexto(contexto){
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', '/imagens/${contexto}.png');
+}
