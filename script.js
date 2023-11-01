f7237a982ce777cbff7b7c39fe8d55f929918287
Letícia Matheus <let.matheus19@hotmail.com>
2023-11-01 15:46:24 -0300

    Adição de uma referência e funções para a adição e remoção do active no click

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
 
 //quando o botão 'focoBt' receber o evento 'click', chamamos uma função
 focoBt.addEventListener('click', () => {
@@ -11,22 +12,30 @@ focoBt.addEventListener('click', () => {
     //html.setAttribute('data-contexto', 'foco');
     //banner.setAttribute('src', '/imagens/foco.png');
     alterarContexto('foco');
+    focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
     //html.setAttribute('data-contexto', 'descanso-curto');
     //banner.setAttribute('src', '/imagens/descanso-curto.png')
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
     //html.setAttribute('data-contexto', 'descanso-longo');
     //banner.setAttribute('src', '/imagens/descanso-longo.png')
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 });
 
 //função para alterar os botões 'junto'
 function alterarContexto(contexto){
+    //para cada botão, eu chamo uma função
+    //a função recebe contexto, que é específico para cada botão e só é ativado quando o botão é clicado
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active');
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
