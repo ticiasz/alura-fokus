af36a89a7c8cc43c214de0796b497f5375865247
Letícia Matheus <let.matheus19@hotmail.com>
2023-11-06 14:14:49 -0300

    Criando as variáveis e a função do temporizador

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('sons/luna-rise-part-one.mp3');
+const startPauseBt = document.querySelector('#start-pause');
+
+let tempoDecorridoEmSegundos = 5;
+
 musica.loop = true;
 
 //quando o botão 'focoBt' receber o evento 'click', chamamos uma função
@@ -72,4 +76,9 @@ function alterarContexto(contexto){
         default:
             break;
     }
+}
+
+const contagemRegressiva = () => {
+    tempoDecorridoEmSegundos -= 1;
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos);
 }
